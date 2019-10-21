571a7730fcd211ba1f22a8d1b31351a4152d5a5d
fernandolucas <fernandolucass.2012@gmail.com>
2019-10-21 17:39:58 -0300

    while

--- while.js
+++ while.js
@@ -1,15 +1,34 @@
-function randomInteger(min, max) {
-   if (min > max) {
-      const temp = min;
-      min = max;
-      max = temp;
-   }
-   const value = Math.random() * ((max + 1) - min) + min;
-   return Math.floor(value);
-}
+// function randomInteger(min, max) {
+//    if (min > max) {
+//       const temp = min;
+//       min = max;
+//       max = temp;
+//    }
+//    const value = Math.random() * ((max + 1) - min) + min;
+//    return Math.floor(value);
+// }
 
-let option = 0;
-while (option != -1) {
-   option = randomInteger(-1, 10);
-   console.log(`number equal ${option}`);
+// let option = 0;
+// while (option != -1) {
+//    option = randomInteger(-1, 10);
+//    console.log(`number equal ${option}`);
+// }
+
+//---------------------------------------------------------------
+
+let contador = 1
+while (contador <= 10) {
+   console.log(`contador  = ${contador}`);
+   contador++;
 }
+//output
+// contador = 1
+// contador = 2
+// contador = 3
+// contador = 4
+// contador = 5
+// contador = 6
+// contador = 7
+// contador = 8
+// contador = 9
+// contador = 10
