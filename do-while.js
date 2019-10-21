d009c4e33027985cbb9c6b7111f13b36e8c494b4
fernandolucas <fernandolucass.2012@gmail.com>
2019-10-21 17:34:43 -0300

    do-while

--- do-while.js
+++ do-while.js
@@ -1,15 +1,15 @@
 function randomInteger(min, max) {
-   if (min > max) {
-      const temp = min;
-      min = max;
-      max = temp;
+   if (max < min) {
+      const temp = max;
+      max = min;
+      min = temp
    }
    const value = Math.random() * ((max + 1) - min) + min;
    return Math.floor(value);
 }
 
-let option = 0;
-while (option != -1) {
-   option = randomInteger(-1, 10);
+let option = -1;
+do {
+   option = randomInteger(-1, 10)
    console.log(`number equal ${option}`);
-}
+} while (option != -1);
