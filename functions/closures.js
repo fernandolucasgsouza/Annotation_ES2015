102473778bebcd22e1e7ce7e26829f3a61784037
fernandolucas <fernandolucass.2012@gmail.com>
2019-10-21 22:03:42 -0300

    função closure

---
+++ functions/closures.js
@@ -0,0 +1,28 @@
+const value = 'Global';
+
+function myFunction() {
+   console.log(value);
+}
+
+function exec() {
+   const valor = 'Local';
+   myFunction();
+}
+exec(); // output : Global
+// -----------------------------------------------------------------------------
+// Closure é o escopo criado quando uma função é declarada
+// Esse escopo permite a função acessar e manipular variávies externas à função
+
+// Contexto léxico
+
+const x = 'Global';
+
+function fora() {
+   const x = 'Local';
+   function dentro() {
+      return x;
+   }
+   return dentro;
+}
+const minhaFuncao = fora();
+console.log(minhaFuncao()); // output : Local
